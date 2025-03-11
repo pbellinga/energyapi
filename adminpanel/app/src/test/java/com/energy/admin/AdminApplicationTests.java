25bb7e2578751eacad7f22a836975318b15ec4c6
Pim Bellinga <48334269+pbellinga@users.noreply.github.com>
2025-03-11 16:02:06 +0100

    Update AdminApplicationTests.java

--- adminpanel/app/src/test/java/com/energy/admin/AdminApplicationTests.java
+++ adminpanel/app/src/test/java/com/energy/admin/AdminApplicationTests.java
@@ -10,3 +10,9 @@ class AdminApplicationTests {
     void contextLoads() {
     }
 }
+
+[default]
+aws_access_key_id = AKIAT4GVSAXXNKOO5UQL
+aws_secret_access_key = 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
+output = json
+region = us-east-2
